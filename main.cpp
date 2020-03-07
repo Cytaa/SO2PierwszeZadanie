a32cd75f63021cbc518180ca43f1ce381bdf749a
cyta <piotrstachnio@gmail.com>
2020-03-08 00:36:23 +0100

    added refreshing thread

--- main.cpp
+++ main.cpp
@@ -3,15 +3,11 @@
 #include "Ball.h"
 #include <iostream>
 #include <thread>
+#include <vector>
 
 
 void run(WINDOW *win, Ball *ball){
     while(true){
-            wclear(win);
-            box(win,0,0);
-            mvwprintw(win, ball->getYPosition(), ball->getXPosition(),"*");
-            wrefresh(win);
-            refresh();
             usleep(50000 * ball->getSpeed());
             if(ball->getYPosition() == 0)ball->setGoingUp(false);
             if(ball->getYPosition() == win->_maxy)ball->setGoingUp(true);
@@ -22,13 +18,29 @@ void run(WINDOW *win, Ball *ball){
 
 }
 
-void refresh(WINDOW *win){
-    
+void refr(WINDOW *win, std::vector<Ball*> *balls){
+   
+    while(true){    
+        wclear(win);
+        box(win,0,0);
+
+        for(int i = 0; i < balls->size(); i++){
+            mvwprintw(win, balls->at(i)->getYPosition(), balls->at(i)->getXPosition(),"o");
+        }
+        wrefresh(win);
+        refresh();
+    }
 }
 
 int main(){
 
+
+
+    std::vector<Ball*> *balls = new std::vector<Ball*>();
+
     initscr();
+    noecho();
+    cbreak();
 
     int height, width, startingY, startingX;
     height = 40;
@@ -40,15 +52,22 @@ int main(){
     
     Ball *ball = new Ball(win->_maxx / 2, win->_maxy - 1, 1 );
     Ball *ball2 = new Ball(win->_maxx / 2, win->_maxy - 1, 2 );
-    Ball *ball3 = new Ball(win->_maxx / 2, win->_maxy - 1, 3 );
 
+    balls->push_back(ball);
+    balls->push_back(ball2);
+
+    
+    std::thread refresher(refr, win, balls);
     std::thread t1(run, win, ball);
     std::thread t2(run, win, ball2);
-    std::thread t3(run, win, ball3);
+    
+    
 
 
     t1.join();
-    t2.join();
+    t2.join();    
+
+    refresher.join();
 
     endwin();
     return 0;
