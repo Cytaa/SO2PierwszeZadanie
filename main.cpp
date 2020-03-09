cf610a27d679c7e455dfe0ea162d7ef4d06db519
cyta <piotrstachnio@gmail.com>
2020-03-09 21:24:41 +0100

    every ball has its own thread

--- main.cpp
+++ main.cpp
@@ -8,17 +8,16 @@
 #include <random>
 
 
-void run(WINDOW *win, std::vector<Ball*> *balls){
+void run(WINDOW *win, Ball *ball){
     
     while(true){
-        for(int i = 0; i < balls->size(); i++){
-            usleep(50000 * balls->at(i)->getSpeed());
-            if(balls->at(i)->getYPosition() == 0)balls->at(i)->setGoingUp(false);
-            if(balls->at(i)->getYPosition() == win->_maxy)balls->at(i)->setGoingUp(true);
-            if(balls->at(i)->getXPosition() == 0)balls->at(i)->setCanLeft(false);
-            if(balls->at(i)->getXPosition() == win->_maxx)balls->at(i)->setCanRight(false);   
-            balls->at(i)->move();
-        }
+        usleep(50000 * ball->getSpeed());
+        if(ball->getYPosition() == 0)ball->setGoingUp(false);
+        if(ball->getYPosition() == win->_maxy)ball->setGoingUp(true);
+        if(ball->getXPosition() == 0)ball->setCanLeft(false);
+        if(ball->getXPosition() == win->_maxx)ball->setCanRight(false);   
+        ball->move();
+
     }
 
 }
@@ -27,24 +26,28 @@ void refr(WINDOW *win, std::vector<Ball*> *balls){
    
     while(true){    
         wclear(win);
-        box(win,0,0);
-
         for(int i = 0; i < balls->size(); i++){
             mvwprintw(win, balls->at(i)->getYPosition(), balls->at(i)->getXPosition(),"o");
         }
         wrefresh(win);
-        refresh();
     }
 }
 
-void newBall(std::vector<Ball*> *balls, WINDOW *win){
+void newBall(std::vector<Ball*> *balls, std::vector<std::thread> *threads,WINDOW *win){
     srand(time(NULL));
-    
+
     while(true){
-        Ball *ball = new Ball(rand() % win->_maxx, rand() % win->_maxy, rand() % 5);
-        balls->push_back(ball);  
+
+        Ball *ball = new Ball(rand() % win->_maxx, rand() % win->_maxy, rand() % 5 + 1);
+        std::thread movement(run, win, ball);
+        balls->push_back(ball);
+        threads->push_back(std::move(movement));
         sleep(2);
+    
     }
+}
+
+void closeProgram(std::vector<std::thread> *threads){
 }
 
 int main(){
@@ -52,10 +55,12 @@ int main(){
 
 
     std::vector<Ball*> *balls = new std::vector<Ball*>();
+    std::vector<std::thread> *threads = new std::vector<std::thread>();
 
     initscr();
     noecho();
     cbreak();
+    curs_set(0);
 
     int height, width, startingY, startingX;
     height = 40;
@@ -65,13 +70,13 @@ int main(){
 
     WINDOW *win = newwin(height, width, startingX, startingY);
     
-    std::thread creator(newBall, balls, win);
-    std::thread move(run, win, balls);
+    std::thread creator(newBall, balls, threads, win);
     std::thread refresher(refr, win, balls);
+    //std::thread close(closeProgram,threads);
     
     refresher.join();
-    move.join();
     creator.join();
+    //close.join();
 
     endwin();
     return 0;
